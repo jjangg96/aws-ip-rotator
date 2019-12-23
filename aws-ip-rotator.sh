f1872d71869312e1c02d63e1e1820fbcad49dd24
이진우 <jjangg96@devignlab.com>
2019-12-23 14:24:59 +0900

    using allocation id

--- aws-ip-rotator.sh
+++ aws-ip-rotator.sh
@@ -11,6 +11,7 @@ case $1 in
 	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 	OLD_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 	NEW_IP=$(aws ec2 allocate-address --query PublicIp | tr -d '"')
+  ALLOC_ID=$(aws ec2 describe-addresses --filters "Name=public-ip,Values=$OLD_IP" | jq -r '.Addresses[0].AllocationId')
 	echo ''
 	echo "Old IP: $OLD_IP"
 	echo "New IP: $NEW_IP"
@@ -18,5 +19,5 @@ case $1 in
 	echo 'Associating new IP...'
 	aws ec2 associate-address --instance-id $INSTANCE --public-ip $NEW_IP
 	echo 'Releasing old IP...'
-	aws ec2 release-address --public-ip $OLD_IP
+	aws ec2 release-address --allocation-id $ALLOC_ID
 esac
